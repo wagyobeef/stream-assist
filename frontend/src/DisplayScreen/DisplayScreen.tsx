cc3b2ebb545eb5e8666fa896f8fb61cd3592e55b
Grant Kim <grant@Grants-MacBook-Pro.local>
2025-05-08 01:07:33 -0700

    yellow

--- frontend/src/DisplayScreen/DisplayScreen.tsx
+++ frontend/src/DisplayScreen/DisplayScreen.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import logo from "../red-cheeks-logo.jpg";
 
 const DisplayScreen: React.FC = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center text-3xl font-sans">
-      Hello
+    <div className="h-screen flex flex-col items-center pt-12 font-sans">
+      <img
+        src={logo}
+        alt="Red Cheeks Logo"
+        style={{
+          width: "12rem",
+          height: "12rem",
+          borderRadius: "9999px",
+          objectFit: "cover",
+          marginBottom: "3.5rem",
+        }}
+      />
+      <span
+        className="font-bold"
+        style={{ color: "#F28B82", fontSize: "3rem" }}
+      >
+        Hello
+      </span>
     </div>
   );
 };
