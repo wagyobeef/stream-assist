c42841231c1f96573d4a2021240a1c3a788c72b4
Grant Kim <grant@Grants-MacBook-Pro.local>
2025-05-08 00:40:49 -0700

    change to cjs

--- frontend/src/DisplayScreen/DisplayScreen.tsx
+++ frontend/src/DisplayScreen/DisplayScreen.tsx
@@ -2,16 +2,7 @@ import React from "react";
 
 const DisplayScreen: React.FC = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        fontSize: "2rem",
-        fontFamily: "Arial, sans-serif",
-      }}
-    >
+    <div className="flex justify-center items-center h-screen text-3xl font-sans bg-[#FFF9DB]">
       Hello
     </div>
   );
