e3bf12820c546dc3500e3dc288ae532996b19edc
Grant Kim <grant@Grants-MacBook-Pro.local>
2025-05-08 02:27:55 -0700

    ig tag

--- frontend/src/DisplayScreen/DisplayScreen.tsx
+++ frontend/src/DisplayScreen/DisplayScreen.tsx
@@ -13,19 +13,26 @@ const DisplayScreen: React.FC = () => {
           height: "12rem",
           borderRadius: "9999px",
           objectFit: "cover",
-          marginBottom: "3.5rem",
+          marginBottom: "2.5rem",
         }}
       />
-      <img
-        src={igLogo}
-        alt="Instagram Logo"
-        style={{ width: "2.5rem", height: "2.5rem", marginBottom: "1rem" }}
-      />
+      <div className="flex flex-row items-center mb-16">
+        <img
+          src={igLogo}
+          alt="Instagram Logo"
+          style={{ width: "2rem", height: "2rem", marginRight: "0.75rem" }}
+        />
+        <span
+          style={{ color: "#ED8C8C", fontSize: "2.25rem", fontWeight: 600 }}
+        >
+          redcheekstcg
+        </span>
+      </div>
       <span
         className="font-bold"
-        style={{ color: "#F28B82", fontSize: "3rem", fontWeight: 900 }}
+        style={{ color: "#3A3A32", fontSize: "3rem", fontWeight: 600 }}
       >
-        Hello
+        $2 start singles
       </span>
     </div>
   );
